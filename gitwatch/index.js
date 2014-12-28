fe5a2465d7ef7fefe126c9d8c86f771296690b4d
naosim <nao_pillows@yahoo.co.jp>
2014-12-28 09:13:32 +0900

    [add] apiにアクセスしてデプロイできる

---
+++ gitwatch/index.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var basicAuth = require('basic-auth-connect');
+app.all('/ctrl/*', basicAuth(function(user, password) {
+  return user === 'user' && password === 'pass' + new Date().getDate();
+}));
+
+app.get('/', function (req, res) {
+  res.send('Hello, World!');
+});
+app.get('/ctrl/deploy', function (req, res) {
+  deploy();
+  res.send('deploy!');
+});
+app.post('/', function (req, res) {
+  res.send('Hello, World!');
+  console.log('post');
+});
+app.listen(3000);
+
+var deploy = function() {
+  if(dayAccessCount.isLocked()) {
+    logger.error('deploy locked!');
+    return;
+  }
+  dayAccessCount.up();
+  logger.log('deploy');
+  var child = exec('sh deploy_force.sh', function(err, stdout, stderr) {
+    if (!err) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+    } else {
+      console.log(err);
+      // err.code will be the exit code of the child process
+      console.log(err.code);
+      // err.signal will be set to the signal that terminated the process
+      console.log(err.signal);
+    }
+  })
+};
+
+var dayAccessCount = (function(max) {
+  var currentDay, count;
+  var isDayChanged = function() { return currentDay != new Date().getDay(); };
+  var reset = function() {
+    currentDay = new Date().getDay();
+    count = 0;
+  };
+  reset();
+  return {
+    up: function() {
+      if(isDayChanged()) reset();
+      count++;
+    },
+    count: function() {
+      if(isDayChanged()) reset();
+      return count;
+    },
+    isLocked: function() {
+      return this.count() >= max;
+    }
+
+  }
+})(5);
+
+var logger = (function() {
+  var getDate = function() {
+    var d = new Date();
+    var seg = [
+      '[',
+      d.getYear() + 1900, '/',
+      d.getMonth() + 1, '/',
+      d.getDate(), ' ',
+      d.getHours(), ':',
+      d.getMinutes(), ':',
+      d.getSeconds(),
+      ']'
+    ];
+    var result = '';
+    for(var i = 0; i < seg.length; i++) result += seg[i];
+    return result;
+  }
+
+  var putDate = function(args) {
+    var result = [getDate()];
+    for(var i = 0; i < args.length; i++) {
+      result[i + 1] = args[i];
+    }
+    return result;
+  }
+  return {
+    log: function() {
+      console.log.apply(console, putDate(arguments));
+    },
+    error: function() {
+      console.error.apply(console, putDate(arguments));
+    }
+  }
+})();
