f4df27dfd5246a5ad8781968dcc888cf4bf99319
naosim <nao_pillows@yahoo.co.jp>
2014-12-28 12:36:36 +0900

    [add] deploy only master

--- deploy_force.sh
+++ deploy_force.sh
@@ -1,3 +1,8 @@
+is_master=`git branch|grep "*"|grep "master"`
+if [ -z "$is_master" ] ; then
+  echo "not master branch. please, switch master."
+  exit 1
+fi
 git pull
 
 FTP_SERVER="your.ftp.server.com"
