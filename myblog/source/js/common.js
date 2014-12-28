3e6d7afbfea1e582f646e0e5e4b77be04ca84e4f
naosim <nao_pillows@yahoo.co.jp>
2014-12-28 16:30:37 +0900

    [clean] jsを外出し

---
+++ myblog/source/js/common.js
@@ -0,0 +1,11 @@
+console.log("common");
+// NINJA ANALYZE
+(function(d) {
+  var sc=d.createElement("script"),
+  ins=d.getElementsByTagName("script")[0];
+  sc.type="text/javascript";
+  sc.src=("https:"==d.location.protocol?"https://":"http://") + "code.analysis.shinobi.jp" + "/ninja_ar/NewScript?id=00257197&hash=b71e4d5e&zone=36";
+  sc.async=true;
+  ins.parentNode.insertBefore(sc, ins);
+})(document);
+//NINJA ANALYZE
